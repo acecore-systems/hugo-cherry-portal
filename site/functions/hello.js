e142b98ecbe6e9d8e63cdd56a9759e3fd14ba365
gnshd <gdev@nshd.info>
2020-07-12 17:48:29 +0900

    Revert "Update functions"
    This reverts commit 8b08630e9bc1e20bef813607af27d1e562d17674.

--- site/functions/hello.js
+++ site/functions/hello.js
@@ -1,18 +1,8 @@
 exports.handler = async (event, context) => {
-  const userdb =  [{'email':'gai@mist.jp','password':'testpass'},{'email':'test@mist.jp','password':'testpass'}]
-
-  const email = event.queryStringParameters.email;
-  const password = event.queryStringParameters.password;
-  const data = { 'email': email, 'password': password }
-  
-  let seccuer = false
-
-  if (userdb.indexOf(data) >= 0) {
-    seccuer = true
-	}
+	const name = event.queryStringParameters.name || "World";
 
 	return {
 		statusCode: 200,
-		body: `seccuer=${seccuer}`
+		body: `Hello, ${name}`
 	};
 };
