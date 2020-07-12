e5b844b4984a9f99af6cf21690735e1fd5f55112
gnshd <gdev@nshd.info>
2020-07-12 17:48:57 +0900

    Revert "Update functions"
    This reverts commit 4e5a5b4e1514297fb54dbf6d521820fd3bc7fe9b.

--- site/functions/hello.js
+++ site/functions/hello.js
@@ -1,9 +1,6 @@
-exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name;
-  const email = event.queryStringParameters.email;
-  const message = event.queryStringParameters.message;
-	return {
-		statusCode: 200,
-		body: `Hello, ${name}, ${email}, ${message}`
-	};
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: 'Hello World',
+  };
 };
